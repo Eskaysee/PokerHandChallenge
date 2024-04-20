426f6f2a706f93846e5035edd31bee714c69bdcf
Sihle Calana <CLNSIH001@myuct.ac.za>
2024-04-20 18:56:32 +0200

    Cleaned up code (renamed variables, added comments, spacing etc), fixed the existing code to respect card strength order of precedence and implemented the remaining card strengths that involve rank repetition (1pair, 2pair, 3 of a kind, etc). All tests passed.

--- src/main/java/com/sprinthive/pokerhands/handrank/HandRankerImpl.java
+++ src/main/java/com/sprinthive/pokerhands/handrank/HandRankerImpl.java
@@ -7,8 +7,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class HandRankerImpl implements HandRanker {
+
+    /**
+     * @param cards list of cards to be ranked
+     * @return the best hand rank for the given cards
+     * */
     @Override
     public HandRank findBestHandRank(List<Card> cards) {
+        //preliminary steps
         if (cards.size() != 5) {
             return new NotRankableHandRanker(cards);
         }
@@ -16,36 +22,78 @@ public class HandRankerImpl implements HandRanker {
         Collections.reverse(cards);
         boolean flush = true;
         boolean straight = true;
-        CardRank hpair, lpair;
-        CardRank rank = cards.get(0).getRank();
-        int matches1 = 0, matches2 = 0;
+        CardRank rank1 = cards.get(0).getRank();
+        CardRank rank2=null, kicker=null;
+        //qty1 quantity of a particular card in the hand
+        int qty1 = 1, qty2 = 0;     //in the case of full house or 2 pair, qty1 is
+                                    //associated with rank2 and qty2 associated with rank1
+
         //gathering the details about the hand: flush, straight & rank repetitions
         for (int i = 1; i < cards.size(); i++) {
-            if (cards.get(i).getSuit() != cards.get(0).getSuit()) {
+            Card currentCard = cards.get(i);
+            Card prevCard = cards.get(i-1);
+            if (flush && currentCard.getSuit() != cards.get(0).getSuit()) {
                 flush = false;
             }
-            if (cards.get(i).getRank().compareTo(cards.get(i-1).getRank()) != -1) {
-                int res = cards.get(i).getRank().compareTo(cards.get(i-1).getRank());
+            if (straight && currentCard.compareTo(prevCard) != -1) {
                 straight = false;
             }
-            if (cards.get(i).getRank() == cards.get(i-1).getRank()) {
-                matches1++;
-            } else if (matches1 > 0 && i != 4) {
-                matches2 = matches1;
-                matches1 = 0;
+
+            if (currentCard.getRank() == prevCard.getRank()) {
+                qty1++;
+            } else if (qty1 > 1 && i != 4) {
+                qty2 = qty1;
+                rank1 = prevCard.getRank();
+                rank2 = currentCard.getRank();
+                qty1 = 1;
+            } else {
+                if (qty1 == 1) {
+                    kicker = prevCard.getRank();
+                    rank2 = currentCard.getRank();
+                } else {
+                    kicker = currentCard.getRank();
+                }
             }
         }
+
         //Determining card strength based on the details gathered above
-        if (flush) {
-            if (straight) {
-                if (rank.equals(CardRank.ACE)) {
-                    return new RoyalFlushHandRank(cards.get(0).getSuit());
-                }
-                return new StraightFlushHandRank(rank);
+        if (flush && straight) {
+            if (rank1 == CardRank.ACE) {
+                return new RoyalFlushHandRank(cards.get(0).getSuit());
             }
+            return new StraightFlushHandRank(rank1);
+        } else if (qty1 == 4) {                 //4 of a kind
+            if (rank1 == kicker) {
+                rank1 = rank2;
+            }
+            return new FourOfAKindHandRank(rank1);
+        } else if (qty1 + qty2 == 5) {            //full house
+            if (qty1>qty2) {
+                return new FullHouseHandRank(rank2, rank1);
+            }
+            return new FullHouseHandRank(rank1, rank2);
+        } else if (flush) {
             return new FlushHandRank(cards);
         } else if (straight) {
-            return new StraightHandRank(rank);
+            return new StraightHandRank(rank1);
+        } else if (qty1 == 3) {                         //3 of a kind
+            return new ThreeOfAKindHandRank(rank2);
+        } else if (qty2 == 3) {
+            return new ThreeOfAKindHandRank(rank1);
+        } else if (qty1 == qty2 && qty1 == 2){            //2 pairs
+            if (rank2.getValue() > rank1.getValue()) {
+                return new TwoPairHandRank(rank2, rank1, kicker);
+            }
+            return new TwoPairHandRank(rank1, rank2, kicker);
+        } else if (qty1 == 2 || qty2 == 2) {           //1 pair
+            CardRank finalRank;
+            if (qty1 == 2) {
+                finalRank = rank2;
+            } else {
+                finalRank = rank1;
+            }
+            cards.removeIf(card -> card.getRank() == finalRank);
+            return new OnePairHandRank(finalRank, cards.stream().map(Card::getRank).toList());
         }
         return new HighCardHandRank(cards);
     }
